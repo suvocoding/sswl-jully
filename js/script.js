f5771ffc9637e49b75d0978210f24f44630a8d5e
suvocoding <66925139+suvocoding@users.noreply.github.com>
2022-10-10 12:51:43 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const hambarger=document.querySelector('.hambarger');
-const navList=document.querySelector('.nav-list');
-hambarger.addEventListener('click',()=>{
-navList.classList.toggle('show');
-});
